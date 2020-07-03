308e13261120c3fe233f408dc203ccab2d07002b
M <staycosy@163.com>
2020-07-03 22:44:02 +0800

    add rsync

---
+++ Nexus/rsync/rsync-resource/inotify.sh
@@ -0,0 +1,12 @@
+host=39.98.153.232
+src=/home/rsync
+des=rsync
+password=/etc/rsyncd.secrets
+user=root
+inotifywait=/usr/local/bin/inotifywait
+
+$inotifywait -mrq --timefmt '%Y%m%d %H:%M' --format '%T %w%f%e' -e modify,delete,create,attrib $src \
+| while read files ; do
+    rsync -avzP --delete  --timeout=100 --password-file=${password} $src $user@$host::$des
+    echo "${files} was rsynced" >>/tmp/rsync.log 2>&1
+done
